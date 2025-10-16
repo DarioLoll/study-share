85dde4299c1d99c917d644fc90cff78ba2d1ef77
Dario Stromajer <115354821+DarioLoll@users.noreply.github.com>
2025-10-16 13:32:26 +0200

    Add login page layout

--- src/public/index.php
+++ src/public/index.php
@@ -8,6 +8,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body data-bs-theme="dark">
-    <h1>Test</h1>
+    <?php include_once '../pages/login-page.php'; ?>
 </body>
 </html>
